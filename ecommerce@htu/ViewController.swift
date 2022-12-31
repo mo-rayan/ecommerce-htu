969df76b6fc15a7e99abb078c7c0707a911d3f8e
rayan <rayanjazazy@gmail.com>
2022-12-31 14:15:55 +0300

    Initial Commit

---
+++ ecommerce@htu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ecommerce@htu
+//
+//  Created by RaYaN on 31/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ecommerce@htuUITests/ecommerce_htuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ecommerce_htuUITestsLaunchTests.swift
+//  ecommerce@htuUITests
+//
+//  Created by RaYaN on 31/12/2022.
+//
+
+import XCTest
+
+final class ecommerce_htuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
